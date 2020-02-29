66f68a70811c01df4fc2623d9dc5701d7390b012
Aishwary Gupta <guptashark@protonmail.com>
2020-02-28 23:17:18 -0500

    Added in the DCB struct
    This DCB struct allows us to
    change the baud rate and other COM
    settings for our open port.

--- main.c
+++ main.c
@@ -5,6 +5,14 @@ int main(void) {
 
 	printf("Better Serial\n");
 
+	DCB dcb_params;
+	dcb_params.DCBlength = sizeof(dcb_params);
+	dcb_params.BaudRate = CBR_9600;
+	dcb_params.ByteSize = 8;
+	dcb_params.StopBits = ONESTOPBIT;
+	dcb_params.Parity = NOPARITY;
+
+
 	HANDLE hComm;
 	hComm = CreateFile(
 			"COM3",
@@ -18,25 +26,28 @@ int main(void) {
 	if (hComm == INVALID_HANDLE_VALUE) {
 		printf("Could not open the port.\n");
 		return 0;
-	} else {
-		printf("Opened the port.\n");
+	}
 
-		char buff[64];
-		WORD nBytes;
+	SetCommState(hComm, &dcb_params);
 
-		ReadFile(
-			hComm,
-			buff,
-			7,
-			&nBytes,
-			NULL );
+	printf("Opened the port.\n");
+
+	char buff[64];
+	DWORD nBytes;
+	LPDWORD p_nBytes = &nBytes;
+
+	ReadFile(
+		hComm,
+		buff,
+		7,
+		p_nBytes,
+		NULL );
 
 
 		printf("reading serial: %s\n", buff);
 
 		CloseHandle(hComm);
 		return 0;
-	}
 
 
 	return 0;
