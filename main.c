0065f87b0dc9b82d66cdc17a39eca8fdd99396ff
Aishwary Gupta <guptashark@protonmail.com>
2020-03-01 19:20:06 -0500

    Move COM settings into a function

--- main.c
+++ main.c
@@ -12,8 +12,19 @@ int establish_port(HANDLE *h_com_ptr) {
 			OPEN_EXISTING,
 			0,
 			NULL );
+
+	return 0;
 }
 
+int config_port(DCB * dcb_params_ptr) {
+
+	dcb_params_ptr->BaudRate = CBR_9600;
+	dcb_params_ptr->ByteSize = 8;
+	dcb_params_ptr->StopBits = ONESTOPBIT;
+	dcb_params_ptr->Parity = NOPARITY;
+
+	return 0;
+}
 
 
 int main(void) {
@@ -21,15 +32,14 @@ int main(void) {
 	printf("Better Serial\n");
 
 	DCB dcb_params;
+
+	// not setting by pointer rn.
 	dcb_params.DCBlength = sizeof(dcb_params);
-	dcb_params.BaudRate = CBR_9600;
-	dcb_params.ByteSize = 8;
-	dcb_params.StopBits = ONESTOPBIT;
-	dcb_params.Parity = NOPARITY;
 
 	HANDLE hComm;
 
 	establish_port( & hComm);
+	config_port( & dcb_params);
 
 	if (hComm == INVALID_HANDLE_VALUE) {
 		printf("Could not open the port.\n");
